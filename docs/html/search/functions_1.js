90ef9126cccba0eac35e27c9865fdb7e1c578f2b
Chris Tralie <chris.tralie@gmail.com>
2022-04-16 17:53:20 -0400

    Updating docs

--- docs/html/search/functions_1.js
+++ docs/html/search/functions_1.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['drawline_13',['drawLine',['../classSimpleCanvas.html#a0d3fab74c4eb69d1ea6a9590afea97e1',1,'SimpleCanvas::drawLine(int x0, int y0, int x1, int y1, uint8_t R, uint8_t G, uint8_t B)'],['../classSimpleCanvas.html#a9948b85688e3f8b010e49e7a0defb677',1,'SimpleCanvas::drawLine(int x0, int y0, int x1, int y1, int thickness, uint8_t R, uint8_t G, uint8_t B)']]],
   ['drawrect_14',['drawRect',['../classSimpleCanvas.html#abc632cab60b786a10ff756b7db90e15e',1,'SimpleCanvas']]],
-  ['drawstring_15',['drawString',['../classSimpleCanvas.html#a380f364b86b27ebaddebc720c2592792',1,'SimpleCanvas']]]
+  ['drawstring_15',['drawString',['../classSimpleCanvas.html#a4f312ceb3de64236b2bdee5a09c93fe4',1,'SimpleCanvas']]]
 ];
